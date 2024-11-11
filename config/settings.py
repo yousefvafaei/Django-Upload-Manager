e21ad09c4d4b360ccd6d90276acd3023643cb43a
yousef vafaei <vafaeiyousef@gmail.com>
2024-11-11 13:15:24 +0330

    Changed database settings to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uploadmanager',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 

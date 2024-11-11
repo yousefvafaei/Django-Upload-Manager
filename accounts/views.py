647553d3746cbc805b0868de550f03a5f2aa396c
yousef vafaei <vafaeiyousef@gmail.com>
2024-11-11 14:09:49 +0330

    Added login and register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.views import View
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import CustomUser
+
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            CustomUser.objects.create_user(**cd)
+            messages.success(request, 'you registered.', 'success')
+            return redirect('home:home')
+
+
+class UserLogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'you logged out successfully', 'success')
+        return redirect('accounts:user_login')
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, email=cd['email'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'info')
+                return redirect('home:home')
+            messages.error(request, 'phone or password is wrong!', 'warning')
